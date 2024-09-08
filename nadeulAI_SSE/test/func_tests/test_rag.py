911dd38280dc0b3c8c22201917c00622c7d1eedd
lee-euiji <leeuj9663@naver.com>
2024-09-08 22:02:44 +0900

    [FEAT] retrieve 함수 Test 까지 완료  (#10)

--- nadeulAI_SSE/test/func_tests/test_rag.py
+++ nadeulAI_SSE/test/func_tests/test_rag.py
@@ -1,18 +1,19 @@
 import pytest
 import time
-from src.requests.rag import retrieve
-from test.test_data import qa_inputs
+from nadeulAI_SSE.src.requests.rag import retrieve
+from nadeulAI_SSE.test.test_data import qa_inputs
 
 # Fixtures
 
 
 # Function Unit Test
 
-@pytest.mark.rag_functest
+# @pytest.mark.rag_functest
 @pytest.mark.parametrize("qa_input", qa_inputs)
-def test_retrieve(qa_input):
+@pytest.mark.asyncio
+async def test_retrieve(qa_input):
     start_time = time.time()
-    output = retrieve(QA=qa_input, task_id=0)
+    output = await retrieve(QA=qa_input, task_id=0)
     elapsed_time = time.time() - start_time
     print(f"output : {output}")
     print(f"elapsed_time : {elapsed_time}")
